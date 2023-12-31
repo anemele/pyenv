b537ab3a2a0feceb956d2e6580c8b43ec40ba66e
Jason Swift <anemele@outlook.com>
2023-12-31 10:11:57 +0800

    update README and setup.bat

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Rust-implemented Virtual enVironment manager
+# Python Virtual env Manager
 
-Based on Python third-party library `virtualenv`
+A Python Virtual environments manager for Windows,
+based on `virtualenv` .
 
 ## Usage
 
@@ -8,14 +9,14 @@ Prerequisites
 
 1. Pre-install some version of Python, and install
 `virtualenv` with executable in PATH.
-2. Set the environment variable `PYTHON_VENV_PATH` to
+2. Set the environment variable `PYTHON_VENV_PATH` as
 where you want to place the envs, and make sure it exists.
 
 Then
 
 There are four subcommands to operate the envs
 
-- `add` create a new env, you can specify an existing Python version
-- `list` alias `ls` list all envs
-- `remove` alias `rm` remove an existing env
-- `use` activate an existing env
+- `add` , create a new env, you can specify an existing Python version
+- `list` , alias `ls` , list all envs
+- `remove` , alias `rm` , remove an existing env
+- `use` , activate an existing env
--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,37 +1,23 @@
 @echo off
 
+set exe=pvm
 @rem where to place the executable?
-copy /y %~dp0pvm.exe %SystemRoot%
+copy /y %~dp0%exe%.exe %SystemRoot%
 
 if not defined PYTHON_VENV_PATH (
-set PYTHON_VENV_PATH="%APPDATA%\Python\venv"
-setx PYTHON_VENV_PATH "%APPDATA%\Python\venv"
+set PYTHON_VENV_PATH="%USERPROFILE%\.local\venv"
+setx PYTHON_VENV_PATH "%USERPROFILE%\.local\venv"
 )
 
 if not exist %PYTHON_VENV_PATH% md %PYTHON_VENV_PATH%
 
-where /q python
-if %errorlevel% == 0 set python=python && goto :boot
-where /q py
-if %errorlevel% == 0 set python=py && goto :boot
-echo NO python.exe found in PATH
-exit /b
+cls
 
-:boot
-set base=%PYTHON_VENV_PATH%\base
-
-if not exist %base% %python% -m venv %base%
-call %base%\Scripts\activate.bat
-
-if not exist %base%\Scripts\pip* python -m ensurepip
-python -m pip install pip --upgrade -q
-
-pip install virtualenv -q
-
-if %errorlevel% neq 0 echo Failed. && exit /b
 echo Done.
-echo Use pvm to
-echo   create a new env: pvm add foo
-echo   list envs: pvm ls
-echo   activate an env: pvm use foo
-echo   remove an env: pvm rm foo
+echo Use %exe% to
+echo   create a new env: %exe% add foo
+echo   list envs: %exe% ls
+echo   activate an env: %exe% use foo
+echo   remove an env: %exe% rm foo
+
+pause
