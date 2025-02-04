ef52c3f39228b67fda43e7a228e8c110f3d4f0c9
Jason Swift <anemele@outlook.com>
2025-02-04 23:39:16 +0800

    complete.

--- src/pyenv/cli.py
+++ src/pyenv/cli.py
@@ -1,8 +1,9 @@
+from pathlib import Path
 from typing import Optional
 
 import click
 
-from .cmd import cmd_add, cmd_list, cmd_remove
+from .cmd import cmd_add, cmd_export, cmd_import, cmd_list, cmd_remove
 
 
 class OrderedGroup(click.Group):
@@ -32,6 +33,20 @@ def cli_list():
 def cli_remove(name: str):
     """Remove an environment"""
     cmd_remove(name)
+
+
+@cli.command(name="export")
+@click.option("-o", "--output", type=Path, help="Output file")
+def cli_export(output: Optional[Path] = None):
+    """Export all environments to a file"""
+    cmd_export(output)
+
+
+@cli.command(name="import")
+@click.option("-i", "--input", type=Path, help="Input file")
+def cli_import(input: Optional[Path] = None):
+    """Import environments from a file"""
+    cmd_import(input)
 
 
 def main():
--- src/pyenv/cmd.py
+++ src/pyenv/cmd.py
@@ -1,6 +1,10 @@
+import re
+import subprocess
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
+from mashumaro.mixins.toml import DataClassTOMLMixin
 from virtualenv.run import cli_run
 
 from .consts import PY_BIN_DIR, PY_ENV_PATH, PY_VENV_CFG
@@ -14,11 +18,11 @@ def env_exists(env_path: Path) -> bool:
     )
 
 
-def cmd_add(name: str, python: Optional[str] = None):
+def cmd_add(name: str, python: Optional[str] = None) -> bool:
     env_path = PY_ENV_PATH / name
     if env_exists(env_path):
         print(f"Virtual environment {name} already exists.")
-        return
+        return False
 
     args = ["--no-setuptools", "--no-wheel", "--no-vcs-ignore"]
     args.append(str(env_path))
@@ -26,13 +30,19 @@ def cmd_add(name: str, python: Optional[str] = None):
         args.extend(["--python", python])
 
     cli_run(args)
+    return True
+
+
+def _list_env_paths():
+    for env_path in PY_ENV_PATH.iterdir():
+        if env_path.is_dir() and env_exists(env_path):
+            yield env_path
 
 
 def cmd_list():
     print("Available virtual environments:")
-    for env_path in PY_ENV_PATH.iterdir():
-        if env_path.is_dir() and env_exists(env_path):
-            print(f"  {env_path.name}")
+    for env_path in _list_env_paths():
+        print(f"  {env_path.name}")
 
 
 def cmd_remove(name: str):
@@ -45,3 +55,66 @@ def cmd_remove(name: str):
 
     shutil.rmtree(env_path)
     print(f"Virtual environment {name} removed.")
+
+
+@dataclass
+class Env:
+    name: str
+    python: str
+    libs: list[str]
+
+
+@dataclass
+class Envs(DataClassTOMLMixin):
+    env: list[Env]
+
+
+ENV_FILE = PY_ENV_PATH / ".envs.toml"
+
+
+def cmd_export(path: Optional[Path] = None):
+    if path is None:
+        path = ENV_FILE
+
+    envs = list[Env]()
+    for env_path in _list_env_paths():
+        cfg_text = (env_path / PY_VENV_CFG).read_text()
+        s = re.search(r"version_info = (\d\.\d{1,2})", cfg_text)
+        if s is None:
+            python = "unknown"
+        else:
+            python = s.group(1)
+
+        lib_path = env_path / "lib"
+        libs = list[str]()
+        for lib in lib_path.glob("**/*.dist-info"):
+            tmp = lib.name.removesuffix(".dist-info")
+            name, version = tmp.rsplit("-", 1)
+            libs.append(f"{name}=={version}")
+
+        env = Env(name=env_path.name, python=python, libs=libs)
+        envs.append(env)
+
+    path.write_text(Envs(env=envs).to_toml())
+    print(f"Virtual environments exported to {path}.")
+
+
+def cmd_import(path: Optional[Path] = None):
+    if path is None:
+        path = ENV_FILE
+    if not path.exists():
+        print(f"Virtual environments file {path} does not exist.")
+        return
+
+    envs = Envs.from_toml(path.read_text())
+    for env in envs.env:
+        if not cmd_add(env.name, python=env.python):
+            continue
+
+        pip_exe = PY_ENV_PATH / env.name / PY_BIN_DIR / "pip.exe"
+        res = subprocess.run(
+            f"{pip_exe} install {' '.join(env.libs)}",
+            capture_output=True,
+        )
+        if res.returncode != 0:
+            print(f"Failed to install packages for {env.name}: {res.stderr}")
