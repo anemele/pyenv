70129d637d40c52f7720f53c58792977a3f62ed7
Jason Swift <anemele@outlook.com>
2025-02-04 23:40:38 +0800

    remove Rust code

--- src/consts.rs
+++
@@ -1,6 +0,0 @@
-pub const PYTHON_VENV_PATH: &str = ".pyenvs";
-pub const PY_VENV_CFG: &str = "pyvenv.cfg";
-#[cfg(target_family = "unix")]
-pub const PY_BIN_DIR: &str = "bin";
-#[cfg(target_family = "windows")]
-pub const PY_BIN_DIR: &str = "Scripts";
